58aea32dd4d3bdee26ece3d65e984d26cef93016
munjungmin <answjdals77@gmail.com>
2023-03-21 23:13:42 +0900

    app.listen() 모듈화

--- login-lecture/app.js
+++ login-lecture/app.js
@@ -14,10 +14,4 @@ app.set("view engine", "ejs");
 
 app.use("/", home);   // use: 미들웨어를 등록해주는 메소드 
 
-app.listen(PORT, () => {
-    console.log("서버 가동");
-});
-
-// routing: 어떤 네트워크 안에서 통신 데이터를 보낼 때 최적의 경로를 선택하는 과정 
-
-
+module.exports = app;
